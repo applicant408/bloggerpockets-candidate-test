2bba861b03e7f8e81a76d0f128b079907a3df9f7
applicant408 <applicant408@example.com>
2019-08-09 22:13:29 +0100

    Serialize the users published posts
    This ensures the users published posts are serialized, matching the HTML format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@
 
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts
+  has_many :posts, -> { published }
 
   # TODO: Validate name, it's used everywhere
   # TODO: Add User#to_s
   # TODO: Emails should be unique
 
   def as_json(_options)
-    super(only: :name)
+    super(only: :name, methods: :posts)
   end
 end
