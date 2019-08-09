a9020f20cc23c94cf1ccee459de1f6e8cde756e1
applicant408 <applicant408@example.com>
2019-08-09 22:40:02 +0100

    Add basic validations to User model
    I'm not sure how far to go with these. To go further than I have would require
    making some assumptions.
    
    We should consider validating the uniqueness of the email addresses and add
    appropriate database constraints.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, -> { published }
 
-  # TODO: Validate name, it's used everywhere
+  validates :name, :email, presence: true
   # TODO: Add User#to_s
-  # TODO: Emails should be unique
 
   def as_json(_options)
     super(only: :name, methods: :posts)
