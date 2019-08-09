ebfe01b115b4cd89307c9d681eadbdf5ceb26cc4
applicant408 <applicant408@example.com>
2019-08-09 19:56:45 +0100

    Resolve SQL injection issue and post sorting
    Previously, there was a SQL injection vulnerability.
    This commit resolves that. It also sets a default, consistent sort order.
    Then we have specific allowlist, asc and desc for sorting posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,8 @@
 
 class PostsController < ApplicationController
   def index
-    @posts = Post.published.includes(:user)
-
-    if params[:sort].present?
-      @posts = @posts.order("created_at #{params[:sort]}") # TODO: Solve SQL injection
-    end
+    @posts = Post.published.includes(:user).order(created_at: :desc)
+    @posts = @posts.reorder(created_at: params[:sort]) if params[:sort].in? %w[asc desc]
   end
 
   def show
